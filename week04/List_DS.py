47f85be851b9139d77d79946d84f5f2c6b2d50ea
kimjiwon <jiwonkim1119@naver.com>
2026-09-23 11:17:48 +0900

    week04 v0.9

---
+++ week04/List_DS.py
@@ -0,0 +1,9 @@
+#artists = []
+artists = list()
+print(artists)
+artists.append("리센느")
+print(artists)
+artists.append("핑클")
+print(artists)
+print(artist.pop(1))
+print(artists)
--- week04/space.py
+++ week04/space.py
@@ -1,7 +1,9 @@
 x = 1
 n = int(input("Input factorial number: "))
+a list = list()
 for i in range(1, n+1):
+    a_list.append(x)
     x = x*i
-print(f"f{n}! = {x}")
-# 0(n) Time complexity
-# 0(1) Time complexity
+print(f"{n}! = {x}")
+print(a_list)
+# 0(n) space complexity
